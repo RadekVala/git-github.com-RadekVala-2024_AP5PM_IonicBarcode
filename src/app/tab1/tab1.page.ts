c1843a8c9fc5a2bf104b404af5ae273dc3fe18c0
vala <vala@utb.cz>
2024-10-22 12:27:17 +0200

    Mock the native scanner functionality for browser

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
 import { Component, runInInjectionContext } from '@angular/core';
 import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint} from '@capacitor/barcode-scanner'
 
+import { Platform } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -10,17 +12,26 @@ export class Tab1Page {
 
   scanResult = ''
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
   async scanBarcode() {
     console.log('scan')
 
-    const result = await CapacitorBarcodeScanner.scanBarcode({
-        hint: CapacitorBarcodeScannerTypeHint.ALL
-    });
-    console.log(result);
+    let scanResult = ''
 
-    this.scanResult = result.ScanResult
+    if(this.platform.is('android')) {
+      // native barcode scanner
+      const result = await CapacitorBarcodeScanner.scanBarcode({
+        hint: CapacitorBarcodeScannerTypeHint.ALL
+      });
+      console.log(result);
+      scanResult = result.ScanResult
+    } else {
+      scanResult = (Math.floor(10000000 + Math.random()) * 90000000).toString() 
+    }
+  
+    // display scan result on UI
+    this.scanResult = scanResult
 
   }
 
