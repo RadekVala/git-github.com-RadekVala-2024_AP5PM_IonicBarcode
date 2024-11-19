0d68e6d1809268e01c7ae8714b18b075b4a157df
vala <vala@utb.cz>
2024-11-19 13:02:17 +0100

    Environment variable with apiKey (do not leave your api key in git repository)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "57216C17F450C2B33DDB6E1C53146AB2",
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "57216C17F450C2B33DDB6E1C53146AB2",
 };
 
 /*
