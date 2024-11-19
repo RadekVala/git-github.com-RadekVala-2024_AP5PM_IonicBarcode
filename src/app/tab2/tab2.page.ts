53093c8bd35439f290e595cb83fe8ab5c4b132bb
vala <vala@utb.cz>
2024-11-19 13:53:58 +0100

    Config for Angular proxy Added

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,37 +1,32 @@
-import { Component } from '@angular/core';
-import { AppStorageService } from '../app-storage.service';
-import { BARCODE_HISTORY } from '../app.constants';
-import { Barcode } from '../model/barcode';
-
+import { Component } from "@angular/core";
+import { AppStorageService } from "../app-storage.service";
+import { BARCODE_HISTORY } from "../app.constants";
+import { Barcode } from "../model/barcode";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  barcodeArray: Barcode[] = [];
 
-  barcodeArray: Barcode[] = []
+  constructor(private appStorage: AppStorageService) {}
 
-  constructor(
-    private appStorage: AppStorageService
-  ) {}
-
-  async ionViewDidEnter () {
-    const data = await this.appStorage.get(BARCODE_HISTORY)
+  async ionViewDidEnter() {
+    const data = await this.appStorage.get(BARCODE_HISTORY);
 
     if (data) {
-      this.barcodeArray = data
+      this.barcodeArray = data;
     }
   }
 
-  deleteBarcode(barcode: Barcode){
-    const index = this.barcodeArray.indexOf(barcode)
+  deleteBarcode(barcode: Barcode) {
+    const index = this.barcodeArray.indexOf(barcode);
 
-    if( index > -1) {
-      this.barcodeArray.splice(index, 1)
-      this.appStorage.set(BARCODE_HISTORY, this.barcodeArray)
+    if (index > -1) {
+      this.barcodeArray.splice(index, 1);
+      this.appStorage.set(BARCODE_HISTORY, this.barcodeArray);
     }
   }
-
 }
