4cbb9734557816fb35e2e55d7aef4b30c9ecd8b6
vala <vala@utb.cz>
2024-11-19 13:53:28 +0100

    REST API call in UpcService implemented

--- src/app/api/upc.service.ts
+++ src/app/api/upc.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment.prod";
+import { UpcResponse } from "../model/upc-response";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class UpcService {
+  private apiUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getData(barcode: string): Observable<UpcResponse> {
+    const url = `${this.apiUrl}/${barcode}`;
+
+    const header = new HttpHeaders({
+      Authorization: `Bearer ${environment.apiKey}`,
+    });
+
+    return this.http.get<UpcResponse>(url, { headers: header });
+  }
 }
