be57b63f2ceb24c630b7b64d3823775c48fe3726
Radek Vala <radek.www@gmail.com>
2024-11-22 11:51:27 +0100

    tab1 - no random code, but testing code generated for testing

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,35 +1,37 @@
-import { Component, runInInjectionContext } from '@angular/core';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint} from '@capacitor/barcode-scanner'
+import { Component, runInInjectionContext } from "@angular/core";
+import {
+  CapacitorBarcodeScanner,
+  CapacitorBarcodeScannerTypeHint,
+} from "@capacitor/barcode-scanner";
 
-import { Platform } from '@ionic/angular';
-import { AppStorageService } from '../app-storage.service';
-import { BARCODE_HISTORY } from '../app.constants';
-import { Barcode } from '../model/barcode';
+import { Platform } from "@ionic/angular";
+import { AppStorageService } from "../app-storage.service";
+import { BARCODE_HISTORY } from "../app.constants";
+import { Barcode } from "../model/barcode";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
-
-  scanResult = ''
-  //barcodeArray: Array<Barcode> = [] 
-  barcodeArray: Barcode[] = [] 
+  scanResult = "";
+  //barcodeArray: Array<Barcode> = []
+  barcodeArray: Barcode[] = [];
 
   constructor(
     private platform: Platform,
-    private appStorage: AppStorageService
+    private appStorage: AppStorageService,
   ) {}
 
   async ionViewDidEnter() {
-    const data = await this.appStorage.get(BARCODE_HISTORY)
+    const data = await this.appStorage.get(BARCODE_HISTORY);
 
-    if(data) {
-      this.barcodeArray = data
+    if (data) {
+      this.barcodeArray = data;
     } else {
       // generate some mock data
-      this.generateMockData()
+      this.generateMockData();
     }
   }
 
@@ -37,39 +39,39 @@ export class Tab1Page {
     const now = new Date();
 
     for (let index = 0; index < 10; index++) {
-      const date = new Date(now.getTime() - index * 24 * 60 * 60 * 1000 )
-      const barcode = new Barcode(this.generateRandomCode(), date)
+      const date = new Date(now.getTime() - index * 24 * 60 * 60 * 1000);
+      const barcode = new Barcode(this.generateRandomCode(), date);
 
       this.barcodeArray.push(barcode);
     }
   }
 
   private generateRandomCode() {
-    return Math.floor(100000000 + Math.random() * 900000000).toString() 
+    return "0062639348571"; // not random, but testing code for https://upcdatabase.org
+    return Math.floor(100000000 + Math.random() * 900000000).toString();
   }
 
   async scanBarcode() {
-    console.log('scan')
+    console.log("scan");
 
-    let scanResult = ''
+    let scanResult = "";
 
-    if(this.platform.is('android') || this.platform.is('ios')) {
+    if (this.platform.is("android") || this.platform.is("ios")) {
       // native barcode scanner
       const result = await CapacitorBarcodeScanner.scanBarcode({
-        hint: CapacitorBarcodeScannerTypeHint.ALL
+        hint: CapacitorBarcodeScannerTypeHint.ALL,
       });
       console.log(result);
-      scanResult = result.ScanResult
+      scanResult = result.ScanResult;
     } else {
-      scanResult = this.generateRandomCode()
+      scanResult = this.generateRandomCode();
     }
-  
+
     // display scan result on UI
-    this.scanResult = scanResult
+    this.scanResult = scanResult;
     const barcode = new Barcode(scanResult);
-    this.barcodeArray.unshift(barcode)
+    this.barcodeArray.unshift(barcode);
 
-    this.appStorage.set(BARCODE_HISTORY, this.barcodeArray)
+    this.appStorage.set(BARCODE_HISTORY, this.barcodeArray);
   }
-
 }
