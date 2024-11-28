6730645e693f4ce226cf16e27436ca674431c40d
vala <vala@utb.cz>
2024-11-28 17:08:12 +0100

    navigate from history to detail

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { UpcService } from "../api/upc.service";
+import { UpcResponse } from "../model/upc-response";
 
 @Component({
   selector: "app-detail",
@@ -9,6 +10,7 @@ import { UpcService } from "../api/upc.service";
 })
 export class DetailPage implements OnInit {
   barcode: string | null = null;
+  upcResponse?: UpcResponse;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,6 +27,7 @@ export class DetailPage implements OnInit {
       this.upcService.getData(unwrappedBarcode).subscribe({
         next: (data) => {
           console.log(data);
+          this.upcResponse = data;
         },
       });
     }
